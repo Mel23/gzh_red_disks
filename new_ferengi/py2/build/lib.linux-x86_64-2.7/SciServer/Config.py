960eac5045cf1c1ef7bae762d328b0e8aa8421d4
Mel <melsimba23@gmail.com>
2016-07-14 11:13:15 -0500

    adding new ferengi notebooks

---
+++ new_ferengi/py2/build/lib.linux-x86_64-2.7/SciServer/Config.py
@@ -0,0 +1,4 @@
+# fix URLs
+CasJobsRESTUri = "http://skyserver.sdss.org/CasJobs/RestApi"
+LoginPortalURL = "http://portal.sciserver.org/login-portal/keystone/v3/tokens"
+SciDriveHost = "http://www.scidrive.org"
