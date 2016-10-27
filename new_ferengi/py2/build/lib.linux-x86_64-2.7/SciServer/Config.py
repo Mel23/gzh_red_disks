7db43e2851384e60b509fa7c554d3b47afb5280c
Mel <melsimba23@gmail.com>
2016-10-27 14:02:30 -0500

    removing py2 folder

--- new_ferengi/py2/build/lib.linux-x86_64-2.7/SciServer/Config.py
+++
@@ -1,4 +0,0 @@
-# fix URLs
-CasJobsRESTUri = "http://skyserver.sdss.org/CasJobs/RestApi"
-LoginPortalURL = "http://portal.sciserver.org/login-portal/keystone/v3/tokens"
-SciDriveHost = "http://www.scidrive.org"
